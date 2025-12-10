b8cd1833e03759198083631e4573e4fe2248206a
Aline Ciane Gomes <106564944+alinecianegomes@users.noreply.github.com>
2025-12-10 14:50:16 -0300

    Update MainActivity.kt

--- -CurrencyConverter/app/src/main/java/com/example/currencyconverter/MainActivity.kt
+++ -CurrencyConverter/app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Inicializa o módulo da aplicação
+
         AppModule.initialize(this)
 
         setContent {
